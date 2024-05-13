d6ca94284cba8c5cabdc34938aa150dbab7f52b5
Isarok <isaac.lucerito@gmail.com>
2024-05-13 12:21:36 +0200

    frontend_feature/products_added_some_cards

--- src/App.jsx
+++ src/App.jsx
@@ -5,28 +5,23 @@ import Register from "./pages/Register";
 import { AuthProvider } from "./context/AuthContext";
 import Home from "./pages/Home";
 import Header from "./components/particles/Header";
-
-
-
+import Products from "./pages/Products";
 
 function App() {
   return (
-     <AuthProvider>
+    <AuthProvider>
       <BrowserRouter>
         <Header />
-       
-      
+
         <Routes>
           <Route path="/" element={<Home />} />
-          
+
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-
-        
-        
+          <Route path="/products" element={<Products />} />
         </Routes>
       </BrowserRouter>
-     </AuthProvider>
+    </AuthProvider>
   );
 }
 
