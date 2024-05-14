08ead7b1241c96bbab64b1ef4dc6a20e5b004645
Lea Montoya <leamontoya19@gmail.com>
2024-05-14 09:58:57 +0200

    frontend_feature/home_slider02

--- src/components/home/Slide.jsx
+++ src/components/home/Slide.jsx
@@ -2,25 +2,25 @@ import React from 'react'
 import Img2 from '../../assets/slide/sponsors.png'
 import Img1 from '../../assets/slide/team.png'
 import Img3 from '../../assets/slide/aftermatch.png'
-import Slider from 'react-slick'
+import Slider  from 'react-slick'
 
 const ImageList = [
   {
     id: 1,
     img: Img1,
-    title: "Bienvenido al Centro Deportivo Ciudad de Los Angeles",
-    description: "",
+    title: "Bienvenido al Centro Deportivo Ciudad de Los Ángeles",
+    description: "Hazte socio y disfruta de todos los benecificios",
   },
   {
     id: 2,
     img: Img2,
-    title: "Bienvenido al Centro Deportivo Ciudad de Los Angeles",
-    description: "Estos son nuestro patrocinadores",
+    title: "Bienvenido al Centro Deportivo Ciudad de Los Ángeles",
+    description: "Conoce quienes son nuestro patrocinadores",
   },
   {
     id: 3,
     img: Img3,
-    title: "Bienvenido al Centro Deportivo Ciudad de Los Angeles",
+    title: "Bienvenido al Centro Deportivo Ciudad de Los Ángeles",
     description: "El fútbol base fomenta el compañerismo",
   }
 
@@ -42,60 +42,56 @@ const Slide = () => {
   };
   
   return (
-  
-  <div className='relative overflow-hidden min-h-[550px]
-  sm:min-h-[650px] bg-gray-l flex justify-center items-center
-   dark:bg-white dark:text-black duration-200'>
-      {/* background pattern */}
-    <div className='h-[700px] w-[700px] bg-gray/40
-     absolute -top-1/2 right-0 rounded-3xl rotate-45 -z[8] '>
-    </div> 
-      {/* slide section */}
+    <div className='relative overflow-hidden min-h-[550px]
+    sm:min-h-[650px] bg-gray-l flex justify-center items-center
+     dark:bg-white dark:text-black duration-200'>
+       {/* background section */}
+      <div className='h-[700px] w-[700px] bg-gray/40
+        absolute -top-1/2 right-0 rounded-3xl rotate-45 -z[8] '>
+      </div>
+      {/* slider section */}
       <div className='container pb-8 sm:pb-0'>
         <Slider {...settings}>
           {ImageList.map((data) =>(
           <div>
+           {/* text content section */}
             <div className='grid grid-cols-1 sm:grid-cols-2'>
-              {/* text content section */}
-              <div
-              className='flex flex-col justify-center
-              gap-4 pt-12 sm:pt-0 text-center sm:text-left
-              order-2 sm:order-1 relative z-10'>
-                <h1 className='text-5xl sm:text-6xl
-                lg:text-7xl font-bold'>
-                  {data.title}
-                </h1>
-                <p className='text-sm'>
+              <div  className='flex flex-col justify-center
+                gap-4 pt-12 sm:pt-0 text-center sm:text-left
+                order-2 sm:order-1 relative z-10'>
+                  <h1 className='text-5xl sm:text-6xl
+                   lg:text-7xl font-bold'>
+                    {data.title}
+                  </h1>
+                  <p className='text-s'>
                   {data.description}
-                </p>
-                <div>
-                  <button className='bg-gradient-to-r from-yellow-d to-beige hover:scale-105
-                  duration-200 text-black py-2 px-4
-                  rounded-full'>
-                  INSCRIPCIONES
-                  </button>
-                </div> 
-              </div>
-              {/* image section */}
-              <div className='order-1 sm:order-2'>
-                <div className='relative z-10'>
-                  <img 
-                  src={data.img} 
-                  alt=""
-                  className='w-[300px] h-[300px] sm:h-[450px] 
-                  sm:w-[450px] sm:scale-125 lg:scale-120
-                  object-contain mx-auto'
-                  />
+                  </p>
+                  <div>
+                    <button 
+                      className='bg-gradient-to-r from-yellow-d to-beige hover:scale-105
+                       duration-200 text-black py-2 px-4
+                       rounded-full'>
+                        Únete a nosostros
+                    </button>
+                  </div>
                 </div>
-              </div>
+                <div className='order-1 sm:order-2'>
+                  <div className='relative z-10'>
+                    <img 
+                     src={data.img}
+                     alt=''
+                     className='w-[300px] h-[300px] sm:h-[450px] 
+                     sm:w-[450px] sm:scale-125 lg:scale-120
+                     object-contain mx-auto'
+                    />
+                  </div>
+                </div>
             </div>
           </div>
-          ))};
+        ))}
         </Slider>
- 
       </div>
     </div>
-  
   );
 };
 
