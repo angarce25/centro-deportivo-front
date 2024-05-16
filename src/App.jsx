69b87375d1e244c0b820d7ddd39f0eeadf3b6651
Isarok <isaac.lucerito@gmail.com>
2024-05-16 10:55:20 +0200

    frontend_develop_fixHeader

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,8 @@ import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import Home from "./pages/Home.jsx";
-import Header from "./components/Home/Header.jsx";
+
 import Administratorr from "./pages/UsersT.jsx";
-
-
 
 import Products from "./pages/Products.jsx";
 import UserP from "./pages/UsersT.jsx";
@@ -15,16 +13,14 @@ import PlayersP from "./pages/PlayersT.jsx";
 import ProductsP from "./pages/ProductsT.jsx";
 
 function App() {
-
   return (
     <AuthProvider>
       <BrowserRouter>
-      <Routes>
-      <Route path="/dashboard/users" element={<UserP />} />
-      <Route path="/dashboard/products" element={<ProductsP />} />
-      <Route path="/dashboard/teams" element={<PlayersP />} />
-      </Routes>
-        <Header />
+        <Routes>
+          <Route path="/dashboard/users" element={<UserP />} />
+          <Route path="/dashboard/products" element={<ProductsP />} />
+          <Route path="/dashboard/teams" element={<PlayersP />} />
+        </Routes>
 
         <Routes>
           <Route path="/" element={<Home />} />
