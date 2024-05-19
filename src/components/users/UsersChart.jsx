43640fda697b377de10b846509e201e85660945a
FiorellaSF <afiosf@gmail.com>
2024-05-19 21:35:22 +0200

    frontend_adminDash_linkwBack01

--- src/components/users/UsersChart.jsx
+++ src/components/users/UsersChart.jsx
@@ -1,62 +1,64 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 function UsersChart() {
-    return (
-        <div className="mt-8">
-            <h2>Padres</h2>
-            <div className="flex flex-col mt-6">
-                <div className="-my-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
-                    <div className="align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200">
-                        <table className="min-w-full">
-                            <thead>
-                                <tr>
-                                    <th className="px-6 py-6 bg-white text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Name</th>
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const apiUrl = import.meta.env.VITE_API_URL; 
+    const extraPath = '/auth/user'; 
+    const fullUrl = apiUrl + extraPath; 
+
+    axios
+      .get(fullUrl)
+      .then((response) => {
+        setUsers(response.data);
+      })
+      .catch((error) => {
+        console.error("Error al obtener los datos de usuarios:", error);
+      });
+  }, []);
+
+  return (
+    
+    <div className="mt-8">
+      <h2>Padres</h2>
+      <div className="flex flex-col mt-6">
+        <div className="-my-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
+          <div className="align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200">
+            <table className="min-w-full">
+              <thead>
+                <tr>
+                <th className="px-6 py-6 bg-white text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Name</th>
                                     <th className="px-6 py-6 bg-white text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Status</th>
                                     <th className="px-6 py-6 bg-white text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Hijos</th>
                                     <th className="px-6 py-6 bg-white text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">DNI</th>
                                     <th className="px-6 py-6 bg-white text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Correo Electrónico</th>
                                     <th className="px-6 py-6 bg-white text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Número de Teléfono</th>
-                                </tr>
-                            </thead>
-
-                            <tbody className="bg-gray-100">
-                                <tr>
-                                    <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-50">Marta Nosé</td>
-                                    <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-50 ">Active</td>
-                                    <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-50 ">2</td>
-                                    <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-50">12345678A</td>
-                                    <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-50">maarta@example.com</td>
-                                    <td className="px-6 py-4 whitespace-no-wrap border-b ">123456789</td>
-                                </tr>
-                                <tr>
-                                    <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-50">Carolina Baeza</td>
-                                    <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-50 ">Inactive</td>
-                                    <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-50 ">3</td>
-                                    <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-50">12345678A</td>
-                                    <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-50">carol@example.com</td>
-                                    <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-50">123456789</td>
-                                </tr>
-                                <tr>
-                                    <td className="px-6 py-4 whitespace-no-wrap border-b border-black-50">Sergio Martinez</td>
-                                    <td className="px-6 py-4 whitespace-no-wrap border-b border-black-50 ">Active</td>
-                                    <td className="px-6 py-4 whitespace-no-wrap border-b border-black-50 ">2</td>
-                                    <td className="px-6 py-4 whitespace-no-wrap border-b border-black-50">12345678A</td>
-                                    <td className="px-6 py-4 whitespace-no-wrap border-b border-black-50">smartinez@example.com</td>
-                                    <td className="px-6 py-4 whitespace-no-wrap border-b border-black-50">123456789</td>
-                                </tr>
-                                <tr>
-                                    <td className="px-6 py-4 whitespace-no-wrap border-b border-black-50">Jesús Molinos</td>
-                                    <td className="px-6 py-4 whitespace-no-wrap border-b border-black-50 ">Active</td>
-                                    <td className="px-6 py-4 whitespace-no-wrap border-b border-black-50 ">1</td>
-                                    <td className="px-6 py-4 whitespace-no-wrap border-b border-black-50">12345678A</td>
-                                    <td className="px-6 py-4 whitespace-no-wrap border-b border-black-50">jesusm@example.com</td>
-                                    <td className="px-6 py-4 whitespace-no-wrap border-b">123456789</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
+                </tr>
+              </thead>
+              <tbody className="bg-gray-100">
+                {users.map((user) => (
+                  <tr key={user._id}>
+                    <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-50">{user.name}</td>
+                    <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-50">{user.status}</td>
+                    <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-50">{user.children}</td>
+                    <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-50">{user.dni}</td>
+                    <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-50">{user.email}</td>
+                    <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-50">{user.mobile}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default UsersChart;
+
+
+
+     
