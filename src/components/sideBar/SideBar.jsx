e31de3cd2fcbd65466e582883bdd82488a5ccc74
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-05-20 10:31:04 +0200

    modifications on sidebar

--- src/components/sideBar/SideBar.jsx
+++ src/components/sideBar/SideBar.jsx
@@ -1,85 +1,281 @@
+
+import React, { useState } from "react";
+import { IoHomeOutline } from "react-icons/io5";
+import { CiShop } from "react-icons/ci";
+import { GiClothes } from "react-icons/gi";
+import { LuUsers2 } from "react-icons/lu";
+import { RiTeamLine } from "react-icons/ri";
+
+import { IoIosMenu } from "react-icons/io";
+import imagen from "../../../public/escudo.png";
+
+function Sidebar() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false); // Nuevo estado para el menú hamburguesa
+
+  return (
+    <>
+      {/* Botón de Menú Hamburguesa */}
+      <button
+        className="lg:hidden block p-2"
+        onClick={() => setMenuOpen(!menuOpen)} // Corrección aquí
+      >
+        <IoIosMenu size={24} />
+      </button>
+
+      {/* Contenedor del Menú Hamburguesa */}
+      <div
+        className={`fixed top-0 left-0 w-full h-full bg-white shadow-md transform transition-transform duration-200 ${
+          menuOpen ? "translate-x-0" : "-translate-x-full"
+        }`}
+        onClick={() => setMenuOpen(false)} // Añadido click fuera para cerrar el menú
+      >
+        
+        {/* Botón para cerrar el menú */}
+        <div className="flex items-center justify-between px-6 py-4">
+          <button onClick={() => setMenuOpen(false)}>X</button>{" "}
+        </div>
+
+        {/* Elementos del Menú */}
+        <div
+          className={`${
+            sidebarOpen ? "block" : "hidden"
+          } fixed inset-0 z-20 transition-opacity bg-black opacity-50 lg:hidden`}
+          onClick={() => setSidebarOpen(false)}
+        ></div>
+
+            <div className="flex items-center justify-center mt-8">
+                <div className="flex items-center">
+                <img className="w-20 h-30" src="../../../public/escudo.png" />
+                </div>
+            </div>
+            <nav className="mt-3 flex justify-center hover:bg-yellow-d transition duration-500">
+                <a
+                className="flex items-center py-2 mt-4 mb-4 text-gray-100  bg-gray-700 bg-opacity-25"
+                href="/dashboard/products"
+                >
+                <IoHomeOutline style={{ fontSize: "25px", fontWeight: "bold" }} />
+                <span className="mx-3">Home</span>
+                </a>
+            </nav>
+            <nav className="mt-3 flex justify-center hover:bg-yellow-d transition duration-500">
+                <a
+                className="flex items-center py-2 mt-4 mb-4 text-gray-100  bg-gray-700 bg-opacity-25"
+                href="/dashboard/products"
+                >
+                <CiShop style={{ fontSize: "25px", fontWeight: "bold" }} />
+                <span className="mx-3 flex justify-center">Tienda</span>
+                </a>
+            </nav>
+            <nav className="mt-3 flex justify-center hover:bg-yellow-d transition duration-500">
+                <a
+                className="flex items-center py-2 mt-4 mb-4 text-gray-100  bg-gray-700 bg-opacity-25"
+                href="/dashboard/products"
+                >
+                <GiClothes style={{ fontSize: "25px", fontWeight: "bold" }} />
+                <span className="mx-3">Productos</span>
+                </a>
+            </nav>
+            <nav className="mt-3 flex justify-center hover:bg-yellow-d transition duration-500">
+                <a
+                className="flex items-center py-2 mt-4 mb-4 text-gray-100  bg-gray-700 bg-opacity-25"
+                href="/dashboard/teams"
+                >
+                <RiTeamLine style={{ fontSize: "25px", fontWeight: "bold" }} />
+                <span className="mx-3">Equipos</span>
+                </a>
+            </nav>
+            <nav className="mt-3 flex justify-center hover:bg-yellow-d transition duration-500">
+                <a
+                className="flex items-center justify-center py-2 mt-4 mb-4 text-gray-100 bg-gray-700 bg-opacity-25"
+                href="/dashboard/users"
+                >
+                <LuUsers2 style={{ fontSize: "25px", fontWeight: "bold" }} />
+                <span className="mx-3 items-center justify-center">Usuarios</span>
+                </a>
+            </nav>
+            <nav className="mt-7 justify-center flex items-center  hover:bg-custom-blue hover:text-white transition duration-500">
+                <a
+                className="flex items-center py-2 mt-4 mb-4 text-gray-100 bg-gray-700 bg-opacity-25"
+                href="/"
+                >
+                <i class="fa-solid fa-arrow-right-from-bracket fa-lg"></i>
+                <span className="mx-3">Cerrar sesión</span>
+                </a>
+            </nav>
+        </div>
+      
+
+      <div
+        className={`${
+          sidebarOpen ? "block" : "hidden"
+        } fixed inset-0 z-20 transition-opacity bg-black opacity-50 lg:hidden`}
+        onClick={() => setSidebarOpen(false)}
+      ></div>
+      <div
+        className={`${
+          sidebarOpen ? "translate-x-0 ease-out" : "-translate-x-full ease-in"
+        } fixed inset-y-0 left-0 z-30 w-64 overflow-y-auto transition duration-300 transform bg-gray-l lg:translate-x-0 lg:static lg:inset-0 h-screen`}
+      >
+        <div className="flex items-center justify-center mt-8">
+          <div className="flex items-center">
+            <img className="w-20 h-30" src="../../../public/escudo.png" />
+          </div>
+        </div>
+        <nav className="mt-3 flex justify-center hover:bg-yellow-d transition duration-500">
+          <a
+            className="flex items-center py-2 mt-4 mb-4 text-gray-100  bg-gray-700 bg-opacity-25"
+            href="/dashboard/products"
+          >
+            <IoHomeOutline style={{ fontSize: "25px", fontWeight: "bold" }} />
+            <span className="mx-3">Home</span>
+          </a>
+        </nav>
+        <nav className="mt-3 flex justify-center hover:bg-yellow-d transition duration-500">
+          <a
+            className="flex items-center py-2 mt-4 mb-4 text-gray-100  bg-gray-700 bg-opacity-25"
+            href="/dashboard/products"
+          >
+            <CiShop style={{ fontSize: "25px", fontWeight: "bold" }} />
+            <span className="mx-3 flex justify-center">Tienda</span>
+          </a>
+        </nav>
+        <nav className="mt-3 flex justify-center hover:bg-yellow-d transition duration-500">
+          <a
+            className="flex items-center py-2 mt-4 mb-4 text-gray-100  bg-gray-700 bg-opacity-25"
+            href="/dashboard/products"
+          >
+            <GiClothes style={{ fontSize: "25px", fontWeight: "bold" }} />
+            <span className="mx-3">Productos</span>
+          </a>
+        </nav>
+        <nav className="mt-3 flex justify-center hover:bg-yellow-d transition duration-500">
+          <a
+            className="flex items-center py-2 mt-4 mb-4 text-gray-100  bg-gray-700 bg-opacity-25"
+            href="/dashboard/teams"
+          >
+            <RiTeamLine style={{ fontSize: "25px", fontWeight: "bold" }} />
+            <span className="mx-3">Equipos</span>
+          </a>
+        </nav>
+        <nav className="mt-3 flex justify-center hover:bg-yellow-d transition duration-500">
+          <a
+            className="flex items-center justify-center py-2 mt-4 mb-4 text-gray-100 bg-gray-700 bg-opacity-25"
+            href="/dashboard/users"
+          >
+            <LuUsers2 style={{ fontSize: "25px", fontWeight: "bold" }} />
+            <span className="mx-3 items-center justify-center">Usuarios</span>
+          </a>
+        </nav>
+        <nav className="mt-7 justify-center flex items-center  hover:bg-custom-blue hover:text-white transition duration-500">
+          <a
+            className="flex items-center py-2 mt-4 mb-4 text-gray-100 bg-gray-700 bg-opacity-25"
+            href="/"
+          >
+            <i class="fa-solid fa-arrow-right-from-bracket fa-lg"></i>
+            <span className="mx-3">Cerrar sesión</span>
+          </a>
+        </nav>
+      </div>
+    </>
+  );
+}
+
+export default Sidebar;
+
+
+
+
+/* ---MODELO ORIGINAL--
 import React, { useState } from 'react';
 import { IoHomeOutline } from "react-icons/io5";
 import { CiShop } from "react-icons/ci";
 import { GiClothes } from "react-icons/gi";
 import { LuUsers2 } from "react-icons/lu";
 import { RiTeamLine } from "react-icons/ri"
+
+import { IoIosMenu } from "react-icons/io";
 import imagen from '../../../public/escudo.png'
 
 function Sidebar() {
     const [sidebarOpen, setSidebarOpen] = useState(false);
-
+    
     return (
-        <>
-            <div
-                className={`${sidebarOpen ? 'block' : 'hidden'} fixed inset-0 z-20 transition-opacity bg-black opacity-50 lg:hidden`}
-                onClick={() => setSidebarOpen(false)}
-            ></div>
-            <div
-                className={`${sidebarOpen ? 'translate-x-0 ease-out' : '-translate-x-full ease-in'} fixed inset-y-0 left-0 z-30 w-64 overflow-y-auto transition duration-300 transform bg-gray-l lg:translate-x-0 lg:static lg:inset-0 h-screen`}
-            >
-                <div className="flex items-center justify-center mt-8">
-                    <div className="flex items-center">
-                        <img className='w-20 h-30' src='../../../public/escudo.png' />
-                    </div>
-                </div>
-                    <nav className="mt-10 flex justify-center hover:bg-yellow-d">
-                    <a
-                        className="flex items-center py-2 mt-4 text-gray-100 hover:bg-yellow-400 bg-gray-700 bg-opacity-25"
-                        href="/dashboard/products"
-                    > 
-                       <IoHomeOutline />
-                        <span className="mx-3">Home</span>
-                    </a>
-                </nav>
-                <nav className="mt-10 flex justify-center hover:bg-yellow-d">
-                    <a
-                        className="flex items-center py-2 mt-4 text-gray-100 hover:bg-yellow-400 bg-gray-700 bg-opacity-25"
-                        href="/dashboard/products"
-                    > 
-                      <CiShop />
-                        <span className="mx-3 flex justify-center">Tienda</span>
-                    </a>
-                </nav>
-                <nav className="mt-10 flex justify-center hover:bg-yellow-d">
-                    <a
-                        className="flex items-center py-2 mt-4 text-gray-100 hover:bg-yellow-400 bg-gray-700 bg-opacity-25"
-                        href="/dashboard/products"
-                    > 
-                        <GiClothes />
-                        <span className="mx-3">Productos</span>
-                    </a>
-                </nav>
-                <nav className="mt-10 flex justify-center hover:bg-yellow-d">
-                    <a
-                        className="flex items-center py-2 mt-4 text-gray-100 hover:bg-yellow-400 bg-gray-700 bg-opacity-25"
-                        href="/dashboard/teams"
-                    > 
-                       <RiTeamLine />
-                        <span className="mx-3">Equipos</span>
-                    </a>
-                </nav>
-                <nav className="mt-10 flex justify-center hover:bg-yellow-d">
-                    <a
-                        className="flex items-center py-2 mt-4 text-gray-100 hover:bg-yellow-400 bg-gray-700 bg-opacity-25"
-                        href="/dashboard/users"
-                    > 
-                      <LuUsers2 />
-                        <span className="mx-3">Usuarios</span>
-                    </a>
-                </nav>
-                <nav className="mt-10 flex justify-center">
-                    <a
-                        className="flex items-center py-2 mt-4 text-gray-100 hover:bg-yellow-400 bg-gray-700 bg-opacity-25"
-                        href="/"
-                    > 
-                        <i class="fa-solid fa-arrow-right-from-bracket"></i>
-                        <span className="mx-3">Cerrar sesión</span>
-                    </a>
-                </nav>
+      <>
+        <div
+          className={`${
+            sidebarOpen ? "block" : "hidden"
+          } fixed inset-0 z-20 transition-opacity bg-black opacity-50 lg:hidden`}
+          onClick={() => setSidebarOpen(false)}
+        ></div>
+        <div
+          className={`${
+            sidebarOpen ? "translate-x-0 ease-out" : "-translate-x-full ease-in"
+          } fixed inset-y-0 left-0 z-30 w-64 overflow-y-auto transition duration-300 transform bg-gray-l lg:translate-x-0 lg:static lg:inset-0 h-screen`}
+        >
+          <div className="flex items-center justify-center mt-8">
+            <div className="flex items-center">
+              <img className="w-20 h-30" src="../../../public/escudo.png" />
             </div>
-        </>
+          </div>
+          <nav className="mt-3 flex justify-center hover:bg-yellow-d transition duration-500">
+            <a
+              className="flex items-center py-2 mt-4 mb-4 text-gray-100  bg-gray-700 bg-opacity-25"
+              href="/dashboard/products"
+            >
+              <IoHomeOutline style={{ fontSize: "25px", fontWeight: "bold" }} />
+              <span className="mx-3">Home</span>
+            </a>
+          </nav>
+          <nav className="mt-3 flex justify-center hover:bg-yellow-d transition duration-500">
+            <a
+              className="flex items-center py-2 mt-4 mb-4 text-gray-100  bg-gray-700 bg-opacity-25"
+              href="/dashboard/products"
+            >
+              <CiShop style={{ fontSize: "25px", fontWeight: "bold" }} />
+              <span className="mx-3 flex justify-center">Tienda</span>
+            </a>
+          </nav>
+          <nav className="mt-3 flex justify-center hover:bg-yellow-d transition duration-500">
+            <a
+              className="flex items-center py-2 mt-4 mb-4 text-gray-100  bg-gray-700 bg-opacity-25"
+              href="/dashboard/products"
+            >
+              <GiClothes style={{ fontSize: "25px", fontWeight: "bold" }} />
+              <span className="mx-3">Productos</span>
+            </a>
+          </nav>
+          <nav className="mt-3 flex justify-center hover:bg-yellow-d transition duration-500">
+            <a
+              className="flex items-center py-2 mt-4 mb-4 text-gray-100  bg-gray-700 bg-opacity-25"
+              href="/dashboard/teams"
+            >
+              <RiTeamLine style={{ fontSize: "25px", fontWeight: "bold" }} />
+              <span className="mx-3">Equipos</span>
+            </a>
+          </nav>
+          <nav className="mt-3 flex justify-center hover:bg-yellow-d transition duration-500">
+            <a
+              className="flex items-center justify-center py-2 mt-4 mb-4 text-gray-100 bg-gray-700 bg-opacity-25"
+              href="/dashboard/users"
+            >
+              <LuUsers2 style={{ fontSize: "25px", fontWeight: "bold" }} />
+              <span className="mx-3 items-center justify-center">Usuarios</span>
+            </a>
+          </nav>
+          <nav className="mt-7 justify-center flex items-center  hover:bg-custom-blue hover:text-white transition duration-500">
+            <a
+              className="flex items-center py-2 mt-4 mb-4 text-gray-100 bg-gray-700 bg-opacity-25"
+              href="/"
+            >
+              <i class="fa-solid fa-arrow-right-from-bracket fa-lg"></i>
+              <span className="mx-3">Cerrar sesión</span>
+            </a>
+          </nav>
+        </div>
+      </>
     );
 }
 
 export default Sidebar;
+*/
