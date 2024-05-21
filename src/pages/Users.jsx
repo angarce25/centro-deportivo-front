f9a8b1138c5f2c560f567b85788bed59bf0a5ad1
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-05-21 10:58:31 +0200

    spinner added to users

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,54 +1,54 @@
-// import React, { useEffect } from 'react';
-// import Sidebar from '../components/sideBar/SideBar.jsx';
-// import UsersChart from '../components/users/UsersChart.jsx';
-// import { useSpinner, Spinner } from "../context/LoadingContext";
-
-// const UserP = () => {
-//   const { loading, setLoading } = useSpinner();
-
-//   useEffect(() => {
-//     setLoading(true); // Activa el spinner
-//     setTimeout(() => {
-//       setLoading(false); // Desactiva el spinner después de cierto tiempo (simulando carga)
-//     }, 1000);
-//   }, [setLoading]);
-
-//   return (
-//     <div>
-//       <Spinner /> {/* Asegúrate de incluir el Spinner aquí */}
-//       {!loading && (
-
-//         <section style={{ display: "flex" }}>
-//           <Sidebar style={{ flex: "0 0 auto" }} /> {/* Fija el Sidebar en la pantalla */}
-//           <div style={{ marginLeft: "20px" }}> {/* Ajusta el espacio entre el Sidebar y los nuevos componentes */}
-//             <UsersChart />
-//             {/* Aquí puedes añadir más componentes, como AddCategory, NewUser, UsersTable, etc. */}
-//           </div>
-//         </section>
-
-//       )}
-//     </div>
-//   );
-// };
-
-// export default UserP;
-
-
-
-
-import Sidebar from '../components/sideBar/SideBar.jsx';
-import UsersChart from '../components/users/UsersChart.jsx';
+import React, { useEffect } from "react";
+import Sidebar from "../components/sideBar/SideBar.jsx";
+import UsersChart from "../components/users/UsersChart.jsx";
+import { useSpinner, Spinner } from "../context/LoadingContext";
 
 const UserP = () => {
+  const { loading, setLoading } = useSpinner();
+
+  useEffect(() => {
+    setLoading(true); // Activa el spinner
+    setTimeout(() => {
+      setLoading(false); // Desactiva el spinner después de cierto tiempo (simulando carga)
+    }, 1000);
+  }, [setLoading]);
+
   return (
-    <section style={{ display: 'flex' }}>
-      <Sidebar style={{ flex: '0 0 auto' }} /> {/* Fija el Sidebar en la pantalla */}
-      <div style={{ marginLeft: '20px' }}> {/* Ajusta el espacio entre el Sidebar y los nuevos componentes */}
-        <UsersChart />
-        {/* Aquí puedes añadir más componentes, como AddCategory, NewUser, UsersTable, etc. */}
-      </div>
-    </section>
+    <div>
+      <Spinner /> {/* Asegúrate de incluir el Spinner aquí */}
+      {!loading && (
+        <section style={{ display: "flex" }}>
+          <Sidebar style={{ flex: "0 0 auto" }} />{" "}
+          {/* Fija el Sidebar en la pantalla */}
+          <div style={{ marginLeft: "20px" }}>
+            {" "}
+            {/* Ajusta el espacio entre el Sidebar y los nuevos componentes */}
+            <UsersChart />
+            {/* Aquí puedes añadir más componentes, como AddCategory, NewUser, UsersTable, etc. */}
+          </div>
+        </section>
+      )}
+    </div>
   );
 };
 
-export default UserP;
+export default UserP;
+
+//import Sidebar from '../components/sideBar/SideBar.jsx';
+//import UsersChart from '../components/users/UsersChart.jsx';
+
+//const UserP = () => {
+//return (
+//<section style={{ display: 'flex' }}>
+//<Sidebar style={{ flex: '0 0 auto' }} /> {/* Fija el Sidebar en la pantalla */}
+//<div style={{ marginLeft: '20px' }}> {/* Ajusta el espacio entre el Sidebar y los nuevos componentes */}
+//<UsersChart />
+{
+  /* Aquí puedes añadir más componentes, como AddCategory, NewUser, UsersTable, etc. */
+}
+//</div>
+//</section>
+//);
+//};
+
+//export default UserP;
