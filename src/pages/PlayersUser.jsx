aea31ce4cd2878922b9c281387a05ab204fe16ac
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-05-21 11:47:49 +0200

    spinner added to Player_users

--- src/pages/PlayersUser.jsx
+++ src/pages/PlayersUser.jsx
@@ -1,16 +1,30 @@
 import PlayersUsersChart from '../components/players/PlayersUsersChart.jsx';
 import Sidebar from '../components/sideBar/SideBar.jsx';
-
+import { useSpinner, Spinner } from "../context/LoadingContext";
+import { React, useEffect } from "react";
 
 const PlayersUser = () => {
+   const { loading, setLoading } = useSpinner();
+
+   useEffect(() => {
+     setLoading(true); // Activa el spinner
+     setTimeout(() => {
+       setLoading(false); // Desactiva el spinner después de cierto tiempo (simulando carga)
+     }, 1000);
+   }, [setLoading]);
+
   return (
-    <section style={{ display: 'flex' }}>
-      <Sidebar style={{ flex: '0 0 auto' }} /> 
-      <div style={{ marginLeft: '20px' }}> 
-        <PlayersUsersChart />
-        
-      </div>
-    </section>
+    <div>
+      <Spinner /> {/* Asegúrate de incluir el Spinner aquí */}
+      {!loading && (
+        <section style={{ display: "flex" }}>
+          <Sidebar style={{ flex: "0 0 auto" }} />
+          <div style={{ marginLeft: "20px" }}>
+            <PlayersUsersChart />
+          </div>
+        </section>
+      )}
+    </div>
   );
 };
 
