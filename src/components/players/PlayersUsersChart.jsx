f5c91f2149af93bb9fd92d9ad6e30c67b91bacae
Abelardo Acosta Cracco <102688833+Moriarty369@users.noreply.github.com>
2024-05-24 18:59:24 +0200

    frontend_feature/user-payments_addedDinamicFontColorChange

--- src/components/players/PlayersUsersChart.jsx
+++ src/components/players/PlayersUsersChart.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
-import axios from 'axios';
-import { useState, useEffect } from 'react';
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function PlayersUserChart() {
   const [myPlayers, setMyPlayers] = useState([]);
@@ -8,36 +8,49 @@ function PlayersUserChart() {
 
   useEffect(() => {
     const apiUrl = import.meta.env.VITE_API_URL; // Obtiene la URL base de la API desde las variables de entorno
-    const playersPath = '/myplayers'; // Añade la parte adicional de la URL
-    const paymentsPath = '/payments'; // Añade la parte adicional de la URL para recoger status de pagos
-    
-    const playersUrl = apiUrl + playersPath; // Combina la URL base con la parte adicional para jugadores
-    const paymentsUrl = apiUrl + paymentsPath; // Combina la URL base con la parte adicional para pagos
+    const extraPath = "/myplayers"; // Añade la parte adicional de la URL
+    const paymentsExtraPath = "/payments"; // Segundo extra path para recoger status de pagos
+    const fullUrl = apiUrl + extraPath; // Combina la URL base con la parte adicional
+    const paymentsFullUrl = apiUrl + paymentsExtraPath;
 
-    const fetchData = async () => {
-      try {
-        // Obtener jugadores
-        const playersResponse = await axios.get(playersUrl, { withCredentials: true });
-        setMyPlayers(playersResponse.data);
+    axios
+      .get(fullUrl, { withCredentials: true })
+      .then((response) => {
+        setMyPlayers(response.data);
+        console.log("Players data:", response.data); // Verifica la estructura de myPlayers
+      })
+      .catch((error) => {
+        console.error("Error al obtener tus jugadores:", error);
+      });
 
-        // Obtener pagos
-        const paymentsResponse = await axios.get(paymentsUrl, { withCredentials: true });
-        setMyPayments(paymentsResponse.data);
-      } catch (error) {
-        console.error("Error al obtener los datos:", error);
-      }
-    };
-
-    fetchData();
+    axios
+      .get(paymentsFullUrl, { withCredentials: true })
+      .then((response) => {
+        setMyPayments(response.data);
+        console.log("Payments data:", response.data); // Verifica la estructura de myPayments
+      })
+      .catch((error) => {
+        console.error("Error al obtener los pagos:", error);
+      });
   }, []);
-  
-  // Aquí podrías combinar la información de jugadores y pagos si es necesario
-  
+
+  const getPaymentStatus = (playerId, paymentType) => {
+    const payment = myPayments.find(
+      (payment) => payment.players_id === playerId
+    );
+    if (payment) {
+      return payment[paymentType]?.status ? "Pagado" : "Pendiente";
+    }
+    return "Pendiente";
+  };
+
   return (
     <section className="mt-8">
       <div className="overflow-x-auto">
         <div className="flex items-center justify-between">
-          <h4 className="text-gray-600 font-bold mb-10">Listado de Jugadores y Pagos</h4>
+          <h4 className="text-gray-600 font-bold mb-10">
+            Listado de Jugadores y Pagos
+          </h4>
         </div>
         <Link to="/dashboard/form-player">
           <button className="text-black font-bold py-2 px-4 rounded bg-yellow-d hover:bg-yellow-l">
@@ -45,7 +58,7 @@ function PlayersUserChart() {
           </button>
         </Link>
       </div>
-  
+
       {/* Tabla para jugadores y pagos */}
       <div className="flex flex-col mt-6">
         <div className="-my-2 overflow-x-auto">
@@ -53,16 +66,37 @@ function PlayersUserChart() {
             <table className="table table-zebra">
               <thead>
                 <tr className="text-gray-800 text-sm">
-                  <th className="px-6 py-6 bg-white text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Nombre</th>
-                  <th className="px-6 py-6 bg-white text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Apellido</th>
-                  <th className="px-6 py-6 bg-white text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Talla Camisa</th>
-                  <th className="px-6 py-6 bg-white text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Talla Pantalón</th>
-                  <th className="px-6 py-6 bg-white text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Talla Zapato</th>
-                  <th className="px-6 py-6 bg-white text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Estado</th>
-                  <th className="px-6 py-6 bg-white text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Pago Anual</th>
-                  <th className="px-6 py-6 bg-white text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Primer Pago</th>
-                  <th className="px-6 py-6 bg-white text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Segundo Pago</th>
-                  <th className="px-6 py-6 bg-white text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Tercer Pago</th>
+                  <th className="px-6 py-6 bg-white text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                    Nombre
+                  </th>
+                  <th className="px-6 py-6 bg-white text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                    Apellido
+                  </th>
+                  <th className="px-6 py-6 bg-white text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                    Talla Camisa
+                  </th>
+                  <th className="px-6 py-6 bg-white text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                    Talla Pantalón
+                  </th>
+                  <th className="px-6 py-6 bg-white text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                    Talla Zapato
+                  </th>
+                  <th className="px-6 py-6 bg-white text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                    Estado
+                  </th>
+                  
+                  <th className="px-6 py-6 bg-white text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                    Primer Pago
+                  </th>
+                  <th className="px-6 py-6 bg-white text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                    Segundo Pago
+                  </th>
+                  <th className="px-6 py-6 bg-white text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                    Tercer Pago
+                  </th>
+                  <th className="px-6 py-6 bg-white text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                    Pago Anual
+                  </th>
                 </tr>
               </thead>
               <tbody>
@@ -86,17 +120,53 @@ function PlayersUserChart() {
                     <td className="px-4 py-4 whitespace-no-wrap border-b border-gray-200">
                       {player.status ? "Activo" : "Inactivo"}
                     </td>
-                    <td className="px-4 py-4 whitespace-no-wrap border-b border-gray-200">
-                      {myPayments.find((payment) => payment.parent_id === player._id)?.annual_payment.status ? "Pagado" : "Pendiente"}
+                    <td
+                      className="px-4 py-4 whitespace-no-wrap border-b border-gray-200"
+                      style={{
+                        color:
+                          getPaymentStatus(player._id, "first_payment") ===
+                          "Pagado"
+                            ? "green"
+                            : "red",
+                      }}
+                    >
+                      {getPaymentStatus(player._id, "first_payment")}
                     </td>
-                    <td className="px-4 py-4 whitespace-no-wrap border-b border-gray-200">
-                      {myPayments.find((payment) => payment.parent_id === player._id)?.first_payment.status ? "Pagado" : "Pendiente"}
+                    <td
+                      className="px-4 py-4 whitespace-no-wrap border-b border-gray-200"
+                      style={{
+                        color:
+                          getPaymentStatus(player._id, "second_payment") ===
+                          "Pagado"
+                            ? "green"
+                            : "red",
+                      }}
+                    >
+                      {getPaymentStatus(player._id, "second_payment")}
                     </td>
-                    <td className="px-4 py-4 whitespace-no-wrap border-b border-gray-200">
-                      {myPayments.find((payment) => payment.parent_id === player._id)?.second_payment.status ? "Pagado" : "Pendiente"}
+                    <td
+                      className="px-4 py-4 whitespace-no-wrap border-b border-gray-200"
+                      style={{
+                        color:
+                          getPaymentStatus(player._id, "third_payment") ===
+                          "Pagado"
+                            ? "green"
+                            : "red",
+                      }}
+                    >
+                      {getPaymentStatus(player._id, "third_payment")}
                     </td>
-                    <td className="px-4 py-4 whitespace-no-wrap border-b border-gray-200">
-                      {myPayments.find((payment) => payment.parent_id === player._id)?.third_payment.status ? "Pagado" : "Pendiente"}
+                    <td
+                      className="px-4 py-4 whitespace-no-wrap border-b border-gray-200"
+                      style={{
+                        color:
+                          getPaymentStatus(player._id, "annual_payment") ===
+                          "Pagado"
+                            ? "green"
+                            : "red",
+                      }}
+                    >
+                      {getPaymentStatus(player._id, "annual_payment")}
                     </td>
                   </tr>
                 ))}
@@ -107,7 +177,6 @@ function PlayersUserChart() {
       </div>
     </section>
   );
-  
 }
 
 export default PlayersUserChart;
