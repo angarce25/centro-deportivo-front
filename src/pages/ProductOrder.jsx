87069f6c90d35f27aace21795d95b245388e2fd9
Carla Escobar <carlaprimola@outlook.es>
2024-05-28 12:43:22 +0200

    frontend_feature/productOrder_createdProductOrder04

--- src/pages/ProductOrder.jsx
+++ src/pages/ProductOrder.jsx
@@ -7,30 +7,47 @@ function ProductOrder() {
   const date = searchParams.get("date");
   const totalProducts = searchParams.get("totalProducts");
   const totalPrice = searchParams.get("totalPrice");
+  const products = JSON.parse(searchParams.get("products"));
 
   return (
     <div className="flex">
       <SideBar />
-      <section className="m-10 card w-150 bg-base-100 shadow-xl">
-        <div className="card-body">
-          <div className="m-10">
-            <h4 className="card-title mb-5">Resumen del Pedido</h4>
-            <p className="mb-3">Fecha pedido: {date}</p>
-            <p className="mb-3">Total de productos: {totalProducts}</p>
-            <p className="mb-3">Precio total: {totalPrice}€</p>
-            <input
-              type="file"
-              className="mt-5 mb-3 file-input file-input-bordered w-full max-w-xs"
-            />
-          </div>
+      <section className="m-10 w-150 bg-base-100 shadow-l flex flex-col md:flex-row justify-between">
+  <div className="m-10 flex-1">
+    <h4 className="card-title mb-5">Resumen del Pedido</h4>
+    <p className="mb-3">Fecha pedido: {date}</p>
 
-          <div className="m-10 card-actions justify-start">
-            <button className="button-register bg-yellow-l hover:bg-yellow-d text-black font-semibold py-2 px-4 rounded">
-              Confirmar pedido
-            </button>
+    {products.map((product) => (
+      <div key={product._id} className="mb-6">
+        <div className="flex items-center mb-3">
+          <img
+            className="w-32 h-32 mr-4"
+            src={product.image}
+            alt={product.name}
+          />
+          <div>
+            <h5 className="card-title">{product.name}</h5>
+            <p className="">Precio: {product.price}€</p>
           </div>
         </div>
-      </section>
+      </div>
+    ))}
+
+    <div className="flex justify-between items-end">
+      <div>
+        <p className="mb-3">Productos totales: {totalProducts}</p>
+        <p className="mb-3">Precio total: {totalPrice}€</p>
+      </div>
+    </div>
+  </div>
+
+  <div className="m-10 card-actions  flex-col md:flex-col md:items-end">
+    <input type="file" className="file-input file-input-bordered file-input-sm w-full max-w-xs mb-10 md:mb-4" />
+    <button className="button-register bg-yellow-l hover:bg-yellow-d text-black font-semibold py-2 px-4 rounded">
+      Confirmar pedido
+    </button>
+  </div>
+</section>
     </div>
   );
 }
