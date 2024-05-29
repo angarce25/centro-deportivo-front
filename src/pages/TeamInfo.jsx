475c55c4fac685c0d7de54dd99e5cfd3574d75ec
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-05-29 10:06:51 +0200

    history of the club modified

--- src/pages/TeamInfo.jsx
+++ src/pages/TeamInfo.jsx
@@ -1,20 +1,19 @@
 import Header from "../components/home/Header.jsx";
 import Footer from "../components/home/Footer";
 import FirstTeam from "../components/teams/FirstTeam.jsx";
+import HeroSlider from "../components/home/SlideExperimental.jsx";
 
 
 function TeamInfo () {
 
 
-  return (    
-  <div >
-  <Header />
-       
-  <FirstTeam />
-  
-  <Footer />
-  </div>
-  )
+  return (
+    <div>
+      <Header />
+        <HeroSlider />
+      <Footer />
+    </div>
+  );
 }
 
 export default TeamInfo;
