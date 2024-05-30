5d3207c774927c7388032bf16959bd13d89e24a3
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-05-30 11:02:19 +0200

    Frontend_develop_footer icons with working links

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,118 +13,101 @@ const Home = () => {
       {/* about us section */}
       <div
         className="min-h[550px] flex justify-center items-center py-12 
-      sm:py-0"
+      sm:py-0 mt-2"
       >
         <div className="container">
           <div
             className="grid grid-cols-1 lg:grid-cols-2
           gap-6 items-center mb-5"
           >
-            <div data-aos="zoom-in">
+            {/* text details section */}
+            <div className="flex flex-col justify-center gap-6 sm:pt-0 mb-5">
+              <h1
+                data-aos="fade-up"
+                className="text-3xl sm:text-3xl font-bold mt-9 ml-10"
+              >
+                ¿Quienes somos?
+              </h1>
+              <hr class="border-t-4 border-yellow-300 mt-2 mb-3 ml-12" />
+
+              <div
+                data-aos="fade-up"
+                className="etxt-sm text.grey tracking-wide leading-5"
+              >
+                <p
+                  className="mt-2 ml-12 mr-12 text-gray-700 text-lg text-justify mb-6 "
+                  style={{ textIndent: "2rem" }}
+                >
+                  El barrio madrileño de la Ciudad de los Ángeles tiene una gran
+                  tradición futbolística. Existían dos grandes equipos: CD
+                  Ciudad de los Ángeles y AD Rayo los Ángeles. Diversos motivos
+                  llevaron a la desaparición de ambos clubes. Para que los
+                  jóvenes del barrio no se quedaran sin practicar su deporte
+                  favorito, en marzo de 2003 nació el Club Deportivo Ciudad de
+                  los Ángeles, con el objetivo de que nuestro barrio siga
+                  representado en los mejores campos de la Comunidad de Madrid y
+                  satisfacer la pasión por el fútbol de nuestros vecinos.
+                </p>
+                <p className="mt-5 ml-12 mr-12 text-gray-700 text-lg text-justify">
+                  En marzo de 2003, un grupo de amigos del barrio de la Ciudad
+                  de los Ángeles (distrito Villaverde) decidió crear este
+                  proyecto con gran ilusión.
+                </p>
+                <p className="mt-5 ml-12 mr-12 text-gray-700 text-lg text-justify">
+                  Así comenzó nuestra historia: un grupo de amigos jugando al
+                  fútbol, comprometidos con el barrio y su gente, apoyados por
+                  los comercios para sacar adelante el equipo económicamente,
+                  transmitiendo sus valores y su amor por este deporte.
+                </p>
+              </div>
+            </div>
+
+            <div data-aos="zoom-in" className="lg:mt-10 md:mt-4 sm:mt-2">
+              <img
+                src={HistoriaClub}
+                alt=""
+                className="max-w-[600px] h-[480px] w-full mx-auto drop-shadow-[-10px_10px_12px_rgba(0,0,0,1)] object-cover rounded-lg"
+              />
+            </div>
+
+            {/* seccion de historia del club */}
+            <div data-aos="zoom-in" className="mb-5 mt-2">
               <img
                 src={ImgAboutus}
                 alt=""
                 className="max-w-[600px] h-[480px] w-full mx-auto drop-shadow-[-10px_10px_12px_rgba(0,0,0,1)] object-cover rounded-lg"
               />
             </div>
-
-            {/* text details section */}
-            <div className="flex flex-col justify-center gap-6 sm:pt-0 mb-5">
-              <h1
-                data-aos="fade-up"
-                className="text-3xl sm:text-4xl font-bold mt-12 ml-14"
-              >
-                ¿Quienes somos?
-              </h1>
-              <div
-                data-aos="fade-up"
-                className="etxt-sm text.grey tracking-wide leading-5"
-              >
-                <p className="mt-2 ml-12 mr-12 text-justify">
-                  Así se escribió el comienzo de nuestra historia, con un grupo
-                  de amigos que se reunían para jugar al fútbol, unidos por su
-                  compromiso con el barrio y su comunidad. Con el respaldo de
-                  los comercios locales, lograron sostener económicamente al
-                  equipo, transmitiendo sus magníficos valores y su profundo
-                  amor por este deporte.
-                </p>
-                <p className="mt-5 ml-12 mr-12 text-justify">
-                  Sin embargo, lo más destacado de nuestro club son nuestros
-                  niños, niñas, adolescentes y adultos, quienes lo consideran un
-                  lugar donde encuentran verdadera felicidad. Para ellos, el
-                  club es mucho más que un campo de juego; es un espacio donde
-                  experimentan alegría, donde encuentran diversión, amistad y
-                  amor por sus compañeros y compañeras. Este crecimiento
-                  constante no ha hecho más que fortalecer los lazos que nos
-                  unen como comunidad, reafirmando nuestro compromiso con el
-                  desarrollo integral de nuestros jugadores y jugadoras. Cada
-                  día, nos esforzamos por mejorar y ofrecer nuevas oportunidades
-                  para que todos los miembros del club puedan alcanzar su máximo
-                  potencial en el deporte que amamos.
-                </p>
-                <p className="mt-5 mb-9 ml-12 mr-12 text-justify">
-                  Este crecimiento constante no ha hecho más que fortalecer los
-                  lazos que nos unen como comunidad, reafirmando nuestro
-                  compromiso con el desarrollo integral de nuestros jugadores y
-                  jugadoras. Cada día, nos esforzamos por mejorar y ofrecer
-                  nuevas oportunidades para que todos los miembros del club
-                  puedan alcanzar su máximo potencial en el deporte que amamos.
-                </p>
-              </div>
-            </div>
-
-            {/* seccion de historia del club */}
             <div className="flex flex-col justify-center gap-6 sm:pt-0 m-0">
-              <h1
-                data-aos="fade-up"
-                className="text-3xl sm:text-4xl font-bold mt-2 ml-14"
-              >
-                Historia del Club
-              </h1>
               <div
                 data-aos="fade-up"
                 className="etxt-sm text.grey tracking-wide leading-5"
               >
-                <p className="mt-0 ml-12 mr-12 text-justify">
-                  El barrio Madrileño de la Ciudad de los Ángeles tiene una gran
-                  tradición futbolística. Existían dos grandes equipos el CD.
-                  Ciudad los Ángeles y la AD. Rayo los Ángeles. Diversos motivos
-                  hicieron que ambos clubes desaparecieran y para que los chicos
-                  de nuestro barrio no se quedasen sin la posibilidad de
-                  practicar su deporte preferido en Marzo de 2003 nació el Club
-                  Deportivo Ciudad de los Ángeles, con el objetivo de que
-                  nuestro barrio siga representado en los mejores Campos de la
-                  Comunidad de Madrid y solucionar ese hambre de fútbol de todos
-                  nuestros vecinos y vecinas.
+                <p className="mt-2 mb-9 ml-12 mr-12 text-gray-700 text-lg text-justify">
+                  Ahora, 21 años después, estamos orgullosos de ver que seguimos
+                  adelante, con equipos en todas las categorías, y que los
+                  chicos y chicas del barrio pueden disfrutar del fútbol
+                  defendiendo la camiseta y el escudo de Ciudad de los Ángeles.
                 </p>
-                <p className="mt-5 ml-12 mr-12 text-justify">
-                  En Marzo de 2003 con una enorme ilusión de un grupo de amigos
-                  del barrio de la Ciudad de los Ángeles (distrito Villaverde),
-                  decidieron crear y creer en este maravilloso proyecto.
+
+                <p className="mt-0 ml-12 mr-12 text-justify text-gray-700 text-lg">
+                  Actualmente el club está es su mejor momento, lo que empezó
+                  siendo un equipo de amigos, ahora se ha convertido en un club
+                  deportivo que celebra entrenamientos todos los días de la
+                  semana y juega sus partidos durante el fin de semana de manera
+                  ininterrumpida.
                 </p>
-                <p className="mt-5 ml-12 mr-12 text-justify">
-                  Así comenzó nuestra historia, un grupo de amigos jugando al
+                <p className="mt-5 ml-12 mr-12 text-justify text-gray-700 text-lg">
+                  En esta temporada 2023/24 contamos 12 equipos, formados por
+                  244 jugadores/as y 23 entrenadores, compitiendo todos ellos en
+                  la Federación de Fútbol de Madrid.
+                </p>
+                <p className="mt-5 mb-10 ml-12 mr-12 text-justify text-gray-700 text-lg">
+                  Así comenzó nuestra historia: un grupo de amigos jugando al
                   fútbol, comprometidos con el barrio y su gente, apoyados por
-                  los comercios para sacar adelante el equipo económicamente,
-                  transmitiendo sus magníficos valores y su amor por este
-                  deporte.
-                </p>
-                <p className="mt-5 mb-5 ml-12 mr-12 text-justify">
-                  Ahora se cumplen 21 años desde que todo empezó y estamos
-                  orgullos de ver que seguimos adelante, que tenemos equipos en
-                  todas las categorías y que los chic@s del barrio pueden
-                  disfrutar del fútbol defendiendo la camiseta y el escudo de su
-                  barrio, Ciudad de los Ángeles.
+                  los comercios para sacar adelante el equipo
                 </p>
               </div>
-            </div>
-
-            <div data-aos="zoom-in" className="mb-5 mt-2">
-              <img
-                src={HistoriaClub}
-                alt=""
-                className="max-w-[600px] h-[480px] w-full mx-auto shadow-xl object-cover rounded-lg"
-              />
             </div>
           </div>
         </div>
