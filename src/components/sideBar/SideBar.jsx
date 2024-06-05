eb695cd2de09dd238ebedb7400b7e6dcb985e35e
Abelardo Acosta Cracco <102688833+Moriarty369@users.noreply.github.com>
2024-06-05 13:24:04 +0200

    frontend_chore/route-protection_fixed-bugs

--- src/components/sideBar/SideBar.jsx
+++ src/components/sideBar/SideBar.jsx
@@ -9,6 +9,7 @@ import imagen from '../../assets/icons/escudo.png';
 import Cookies from 'js-cookie';
 import Swal from 'sweetalert2';
 import { useLocation } from 'react-router-dom';
+import {jwtDecode} from 'jwt-decode';
 
 function Sidebar() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -18,49 +19,45 @@ function Sidebar() {
   const location = useLocation();
 
   useEffect(() => {
-    const adminStatus = Cookies.get('isAdmin') === 'true';
-    setIsAdmin(adminStatus);
-   
-    const userCookieName = Cookies.get('name');
-    if (userCookieName) {
-      setUserName(userCookieName);
-      if (!welcomeShown && location.pathname === '/') {
-        Swal.fire({
-          icon: 'success',
-          title: `<span style="font-size: 18px;">¡Bienvenido, ${userCookieName}!</span>`,
-          showConfirmButton: false,
-          timer: 1500
-        });
-        setWelcomeShown(true);
+    const token = Cookies.get('token');
+    if (token) {
+      try {
+        const decodedToken = jwtDecode(token);
+        setIsAdmin(decodedToken.isAdmin ? 'admin' : 'user');
+        console.log('DECODED TOKEN EN SIDEBAR:', decodedToken)
+        setUserName(decodedToken.username); // Assuming you store the username in the token
+      } catch (error) {
+        console.error("Error decoding token:", error);
       }
     }
-  }, [welcomeShown, location.pathname]);
+
+    if (userName && !welcomeShown && location.pathname === '/') {
+      Swal.fire({
+        icon: 'success',
+        title: `<span style="font-size: 18px;">¡Bienvenido, ${userName}!</span>`,
+        showConfirmButton: false,
+        timer: 1500
+      });
+      setWelcomeShown(true);
+    }
+  }, [welcomeShown, location.pathname, userName]);
 
   const renderUserLinks = () => (
     <>
       <nav className="mt-3 flex justify-center hover:bg-yellow-d transition duration-500">
-        <a
-          className="flex items-center py-2 mt-4 mb-4 text-gray-100 bg-gray-700 bg-opacity-25"
-          href="/dashboard/products"
-        >
+        <a className="flex items-center py-2 mt-4 mb-4 text-gray-100 bg-gray-700 bg-opacity-25" href="/dashboard/products">
           <GiClothes style={{ fontSize: "25px", fontWeight: "bold" }} />
           <span className="mx-3">Productos</span>
         </a>
       </nav>
       <nav className="mt-3 flex justify-center hover:bg-yellow-d transition duration-500">
-        <a
-          className="flex items-center py-2 mt-4 mb-4 text-gray-100 bg-gray-700 bg-opacity-25"
-          href="/dashboard/my-players"
-        >
+        <a className="flex items-center py-2 mt-4 mb-4 text-gray-100 bg-gray-700 bg-opacity-25" href="/dashboard/my-players">
           <RiTeamLine style={{ fontSize: "25px", fontWeight: "bold" }} />
           <span className="mx-3">Jugadores</span>
         </a>
       </nav>
       <nav className="mt-3 flex justify-center hover:bg-yellow-d transition duration-500">
-        <a
-          className="flex items-center py-2 mt-4 mb-4 text-gray-100 bg-gray-700 bg-opacity-25"
-          href="/dashboard/myorders"
-        >
+        <a className="flex items-center py-2 mt-4 mb-4 text-gray-100 bg-gray-700 bg-opacity-25" href="/dashboard/myorders">
           <LuUsers2 style={{ fontSize: "25px", fontWeight: "bold" }} />
           <span className="mx-3 items-center justify-center">Mis pedidos</span>
         </a>
@@ -71,28 +68,19 @@ function Sidebar() {
   const renderAdminLinks = () => (
     <>
       <nav className="mt-3 flex justify-center hover:bg-yellow-d transition duration-500">
-        <a
-          className="flex items-center py-2 mt-4 mb-4 text-gray-100 bg-gray-700 bg-opacity-25"
-          href="/dashboard/users"
-        >
+        <a className="flex items-center py-2 mt-4 mb-4 text-gray-100 bg-gray-700 bg-opacity-25" href="/dashboard/users">
           <CiShop style={{ fontSize: "25px", fontWeight: "bold" }} />
           <span className="mx-3 flex justify-center">Usuarios</span>
         </a>
       </nav>
       <nav className="mt-3 flex justify-center hover:bg-yellow-d transition duration-500">
-        <a
-          className="flex items-center py-2 mt-4 mb-4 text-gray-100 bg-gray-700 bg-opacity-25"
-          href="/dashboard/players"
-        >
+        <a className="flex items-center py-2 mt-4 mb-4 text-gray-100 bg-gray-700 bg-opacity-25" href="/dashboard/players">
           <LuUsers2 style={{ fontSize: "25px", fontWeight: "bold" }} />
           <span className="mx-3 items-center justify-center">Jugadores</span>
         </a>
       </nav>
       <nav className="mt-3 flex justify-center hover:bg-yellow-d transition duration-500">
-        <a
-          className="flex items-center py-2 mt-4 mb-4 text-gray-100 bg-gray-700 bg-opacity-25"
-          href="/dashboard/orders"
-        >
+        <a className="flex items-center py-2 mt-4 mb-4 text-gray-100 bg-gray-700 bg-opacity-25" href="/dashboard/orders">
           <LuUsers2 style={{ fontSize: "25px", fontWeight: "bold" }} />
           <span className="mx-3 items-center justify-center">Pedidos</span>
         </a>
@@ -122,7 +110,7 @@ function Sidebar() {
           </a>
         </nav>
 
-        {isAdmin ? renderAdminLinks() : renderUserLinks()}
+        {isAdmin === 'admin' ? renderAdminLinks() : renderUserLinks()}
 
         <nav className="mt-7 justify-center flex items-center hover:bg-custom-blue hover:text-white transition duration-500">
           <a className="flex items-center py-2 mt-4 mb-4 text-gray-100 bg-gray-700 bg-opacity-25" href="/">
--- src/context/PrivateRoutes.jsx
+++ src/context/PrivateRoutes.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
-import { jwtDecode } from "jwt-decode";
+// import { jwtDecode } from "jwt-decode";
 import Cookies from 'js-cookie';
 
 const getTokenFromCookies = () => {
   return Cookies.get('token');
 };
-const decodeToken = (token) => {
-  try {
-    return jwtDecode(token);
-  } catch (error) {
-    console.error("Token decoding failed:", error);
-    return null;
-  }
-};
+// const decodeToken = (token) => {
+//   try {
+//     return jwtDecode(token);
+//   } catch (error) {
+//     console.error("Token decoding failed:", error);
+//     return null;
+//   }
+// };
 
 export const PrivateRoute = ({ redirectPath = "/login" }) => {
   const token = getTokenFromCookies();
@@ -34,9 +34,7 @@ console.log('ROL DE ADMIN EN PRIVATE ROUTE: ', isAdmin)
   if (!token) {
     return <Navigate to={redirectPath} replace />;
   }
-
-
-  if (isAdmin !== true ) {
+  if (isAdmin !== 'admin' ) {
     return <Navigate to={redirectPath} replace />;
   }
 
