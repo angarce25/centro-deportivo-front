5386500f250cb1d42a0cd99942e31bbba6746113
Lea Montoya <leamontoya19@gmail.com>
2024-06-07 09:40:15 +0200

    frontend_feature/users-and-players-view_team-assigned

--- src/components/players/PlayersUsersChart.jsx
+++ src/components/players/PlayersUsersChart.jsx
@@ -1,4 +1,3 @@
-
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { useState, useEffect } from "react";
@@ -81,6 +80,9 @@ function PlayersUserChart() {
                     Edad
                   </th>
                   <th className="px-6 py-6 bg-white text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                    Equipo
+                  </th>
+                  <th className="px-6 py-6 bg-white text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
                     Talla Camisa
                   </th>
                   <th className="px-6 py-6 bg-white text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
@@ -124,6 +126,9 @@ function PlayersUserChart() {
                       </td>
                       <td className="px-4 py-4 whitespace-no-wrap border-b border-gray-200 text-center">
                         {player.age} años
+                      </td>
+                      <td className="px-4 py-4 whitespace-no-wrap border-b border-gray-200 text-center">
+                        {player.team ? player.team.name : "Pendiente"}
                       </td>
                       <td className="px-4 py-4 whitespace-no-wrap border-b border-gray-200 text-center">
                         {player.shirt_size}
