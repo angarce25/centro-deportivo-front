9796f89504453fc0b5e2cd0bd218d5e0631675db
Abelardo Acosta Cracco <102688833+Moriarty369@users.noreply.github.com>
2024-06-16 13:33:24 +0200

    frontend_e2e_added basic e2e UI (to fix: bug 401 mainly in backend)

--- src/App.jsx
+++ src/App.jsx
@@ -14,74 +14,101 @@ import Merchandising from "./pages/Merchandising.jsx";
 import { PlayerProvider } from "./context/PlayerContext.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import { SpinnerProvider } from "./context/LoadingContext.jsx";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import MyOrders from "./pages/MyOrders.jsx";
 import TeamInfo from "./pages/TeamInfo.jsx";
 import { PrivateRoute, AdminPrivateRoute } from "./context/PrivateRoutes.jsx";
 import OrdersAdmin from "./pages/OrdersAdmin.jsx";
-import NotFound from "./components/notfound/NotFound.jsx"
+import NotFound from "./components/notfound/NotFound.jsx";
 import OrdersUsersChart from "./components/orders/OrdersUsersChart.jsx";
 import AdminMemberships from "./pages/AdminMemberships.jsx";
 import MyPayment from "./pages/MyPayment.jsx";
 
-
-
 function App() {
-  
   return (
     <AuthProvider>
-    <SpinnerProvider>
-      <ToastContainer />
-      <ShoppingCartProvider>
-        <PlayerProvider>
-          <BrowserRouter>
-            {/* RUTAS NAVBAR */}
-            
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/info" element={<TeamInfo />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/merchandising" element={<Merchandising />} />
-              <Route path="/*" element={<NotFound />} />
+      <SpinnerProvider>
+        <ToastContainer />
+        <ShoppingCartProvider>
+          <PlayerProvider>
+            <BrowserRouter>
+              {/* RUTAS NAVBAR */}
 
-              {/* Rutas protegidas para el usuario */}
-              <Route element={<PrivateRoute isAllowed={false} />}>
-                <Route path="/dashboard/my-players" element={<PlayersUser />} />
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/info" element={<TeamInfo />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/merchandising" element={<Merchandising />} />
+                <Route path="/*" element={<NotFound />} />
 
-                <Route path="/dashboard/my-memberships" element={<UserMemberships />} />
-                <Route path="/dashboard/payment/:playerId" element={<MyPayment />} /> 
 
-                <Route path="/dashboard/myorders" element={<MyOrders />} />
-                <Route path="/dashboard/form-player" element={<NewPlayer />} />
-                <Route path="/dashboard/products" element={<Products />} />
-                <Route path="/dashboard/add-order" element={<ProductOrder />} />
-                <Route
-                  path="/dashboard/product-order"
-                  element={<ProductOrder />}
-                />
-              </Route>
-              {/* Final de las Rutas protegidas para el usuario */}
 
-              {/* Rutas protegidas para el Administrador */}
-              <Route element={<AdminPrivateRoute isAllowed={false} />}>
-                <Route path="/dashboard/users" element={<UserP />} />
-                <Route
-                  path="/dashboard/players"
-                  element={<PlayersAdmin />}
-                />{" "}
-                <Route path="/dashboard/orders" element={<OrdersAdmin />} />
-                <Route path="/dashboard/teams" element={<PlayersUser />} />
-                <Route path="/dashboard/memberships" element={<AdminMemberships />} />
-              </Route>
-              {/* Final de rutas protegidas para el Administrador */}
 
-            </Routes>
-          </BrowserRouter>
-        </PlayerProvider>
-      </ShoppingCartProvider>
-    </SpinnerProvider>
+
+
+                <Route path="/verify-code" element={<NotFound />} />
+
+
+
+
+
+
+
+                {/* Rutas protegidas para el usuario */}
+                <Route element={<PrivateRoute isAllowed={false} />}>
+                  <Route
+                    path="/dashboard/my-players"
+                    element={<PlayersUser />}
+                  />
+
+                  <Route
+                    path="/dashboard/my-memberships"
+                    element={<UserMemberships />}
+                  />
+                  <Route
+                    path="/dashboard/payment/:playerId"
+                    element={<MyPayment />}
+                  />
+
+                  <Route path="/dashboard/myorders" element={<MyOrders />} />
+                  <Route
+                    path="/dashboard/form-player"
+                    element={<NewPlayer />}
+                  />
+                  <Route path="/dashboard/products" element={<Products />} />
+                  <Route
+                    path="/dashboard/add-order"
+                    element={<ProductOrder />}
+                  />
+                  <Route
+                    path="/dashboard/product-order"
+                    element={<ProductOrder />}
+                  />
+                </Route>
+                {/* Final de las Rutas protegidas para el usuario */}
+
+                {/* Rutas protegidas para el Administrador */}
+                <Route element={<AdminPrivateRoute isAllowed={false} />}>
+                  <Route path="/dashboard/users" element={<UserP />} />
+                  <Route
+                    path="/dashboard/players"
+                    element={<PlayersAdmin />}
+                  />{" "}
+                  <Route path="/dashboard/orders" element={<OrdersAdmin />} />
+                  <Route path="/dashboard/teams" element={<PlayersUser />} />
+                  <Route
+                    path="/dashboard/memberships"
+                    element={<AdminMemberships />}
+                  />
+                </Route>
+                {/* Final de rutas protegidas para el Administrador */}
+              </Routes>
+            </BrowserRouter>
+          </PlayerProvider>
+        </ShoppingCartProvider>
+      </SpinnerProvider>
     </AuthProvider>
   );
 }
--- src/components/forms/Logincomponent.jsx
+++ src/components/forms/Logincomponent.jsx
@@ -1,84 +1,213 @@
+// import axios from 'axios';
+// import Cookies from 'js-cookie';
+// import { useState } from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import { toast } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+
+// const API = import.meta.env.VITE_API_URL;
+
+// // eslint-disable-next-line react/prop-types
+// const Logincomponent = ({ onFormSwitch }) => {
+//   const [email, setEmail] = useState('');
+//   const [pass, setPass] = useState('');
+//   const navigate = useNavigate();
+
+//   const handleCookie = (token, isAdmin) => {
+//     // Almacena el token y el rol en las cookies
+//     Cookies.set('token', token, { expires: 1 }); 
+//     Cookies.set('isAdmin', isAdmin, { expires: 1 });
+//     // console.log('Token para la cookie:', token);
+//     // console.log('Cookie establecida:', Cookies.get('token'));
+//   };
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+  
+//     try {
+//       const response = await axios.post(`${API}/login`, { email, password: pass });
+//   // console.log('RESPONSE.DATA EN LOGINCOMPONENT: ', response.data)
+//   const { token, isAdmin, username: name } = response.data; 
+  
+//   // console.log('Token recibido:', token);
+//   // console.log('Role recibido:', isAdmin);
+//   // console.log('Nombre recibido:', name);
+
+//   if (token) {
+//     handleCookie(token, isAdmin, name);
+
+//     // Mostrar notificación de bienvenida
+//     toast.success(`¡Bienvenido, ${name}!`, {
+//       toastStyle: {
+//         marginTop: "12rem",
+//       },
+//     });
+
+//     setTimeout(() => {
+//       if (isAdmin === 'admin') {
+//         navigate('/dashboard/users');
+//       } else if (isAdmin === 'user'){
+//         navigate('/dashboard/my-players');
+//       }
+//     }, 2000);
+//   }
+// } catch (error) {
+//   if (error.response) {
+//     console.error("Error al iniciar sesión:", error.response.data.message || 'Error en el inicio de sesión');
+
+//     if (error.response.status === 429) {
+//       toast.error("Demasiados intentos de inicio de sesión. Por favor, inténtelo de nuevo más tarde.");
+//     } else {
+//       toast.error("Error en inicio de sesión");
+//     }
+//   } else if (error.request) {
+//     console.error("Error al iniciar sesión:", 'No se pudo conectar con el servidor');
+//     toast.error("Error en inicio de sesión");
+//   } else {
+//     console.error("Error al iniciar sesión:", 'Error al procesar la solicitud de inicio de sesión');
+//     toast.error("Error en inicio de sesión");
+//   }
+// }
+// };
+  
+//   return (
+//     <div    
+//       style={{
+//       maxWidth: "100%",
+//       height: "auto",
+//       filter: "drop-shadow(10px 10px 12px rgba(90, 90, 0, 0.8))",
+//     }}
+//     className=" bg-gray-200 min-h-screen flex items-center justify-center">
+//       <div className="bg-white p-8 rounded-xl shadow-md w-full max-w-md mb-12 " style={{ backgroundColor: "#F2F2F2" }}>
+//         <h2 className="text-3xl font-semibold text-center mb-6">Iniciar sesión</h2>
+//         <form className="space-y-4" onSubmit={handleSubmit}>
+//           <div className="flex flex-col items-center">
+//             <label htmlFor="email" className="block mb-1 text-center font-bold">Correo electrónico</label>
+//             <input 
+//               value={email} 
+//               onChange={(e) => setEmail(e.target.value)} 
+//               type="email" 
+//               placeholder="Correo electrónico"  
+//               id="email" 
+//               autoComplete="off"
+//               name="email" 
+//               required 
+//               className="input w-full max-w-xs rounded-xl"  
+//               style={{ borderRadius: "5px" }} 
+//             />
+//           </div>
+//           <div className="flex flex-col items-center">
+//             <label htmlFor="password" className="block mb-1 text-center font-bold">Contraseña</label>
+//             <input 
+//               value={pass} 
+//               onChange={(e) => setPass(e.target.value)} 
+//               type="password" 
+//               placeholder="******" 
+//               autoComplete="off"
+//               id="password" 
+//               name="password" 
+//               minLength="6" 
+//               required
+//               className="input w-full max-w-xs rounded-xl"  
+//               style={{ borderRadius: "5px" }} 
+//             />
+//           </div>
+//           <div className="flex justify-center">
+//             <button 
+//               className="button-login  bg-yellow-l hover:bg-yellow-d text-black font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline" 
+//               type="submit" 
+              
+//             >
+//               Iniciar sesión
+//             </button>
+//           </div>
+//         </form>
+//         <button 
+//           className="link-btn mt-4 text-center font-bold text-yellow-400 hover:text-yellow-500" 
+//           onClick={() => onFormSwitch("Registercomponent")} 
+//           style={{ color: "#142740", display: "block", margin: "0 auto" }}
+//         >
+//           <small>¿No tienes cuenta? Regístrate aquí</small>
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Logincomponent;
+
+
+
+
 import axios from 'axios';
 import Cookies from 'js-cookie';
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom'; // Asegúrate de importar useNavigate
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const API = import.meta.env.VITE_API_URL;
 
-// eslint-disable-next-line react/prop-types
-const Logincomponent = ({ onFormSwitch }) => {
-  const [email, setEmail] = useState('');
-  const [pass, setPass] = useState('');
+const LoginComponent = ({ onFormSwitch }) => {
   const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [verificationCode, setVerificationCode] = useState('');
+  const [needsVerificationCode, setNeedsVerificationCode] = useState(false);
 
-  const handleCookie = (token, isAdmin) => {
-    // Almacena el token y el rol en las cookies
-    Cookies.set('token', token, { expires: 1 }); 
-    Cookies.set('isAdmin', isAdmin, { expires: 1 });
-    // console.log('Token para la cookie:', token);
-    // console.log('Cookie establecida:', Cookies.get('token'));
+  const handleCookie = (token, isAdmin, name) => {
+    Cookies.set('token', token, { expires: 1 });
+    Cookies.set('isAdmin', isAdmin ? 'admin' : 'user', { expires: 1 });
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
+
     try {
-      const response = await axios.post(`${API}/login`, { email, password: pass });
-  // console.log('RESPONSE.DATA EN LOGINCOMPONENT: ', response.data)
-  const { token, isAdmin, username: name } = response.data; 
-  
-  // console.log('Token recibido:', token);
-  // console.log('Role recibido:', isAdmin);
-  // console.log('Nombre recibido:', name);
+      if (needsVerificationCode) {
+        const response = await axios.post(`${API}/verify-code`, { email, code: verificationCode });
+        const { token, isAdmin, username: name, message } = response.data;
 
-  if (token) {
-    handleCookie(token, isAdmin, name);
+        if (token) {
+          handleCookie(token, isAdmin, name);
+          toast.success(message);
 
-    // Mostrar notificación de bienvenida
-    toast.success(`¡Bienvenido, ${name}!`, {
-      toastStyle: {
-        marginTop: "12rem",
-      },
-    });
+          setTimeout(() => {
+            if (isAdmin === 'admin') {
+              navigate('/dashboard/users');
+            } else if (isAdmin === 'user') {
+              navigate('/dashboard/my-players');
+            }
+          }, 2000);
+        }
+      } else {
+        const response = await axios.post(`${API}/login`, { email, password });
+        const { requires2f, token, isAdmin, username: name, message } = response.data;
 
-    setTimeout(() => {
-      if (isAdmin === 'admin') {
-        navigate('/dashboard/users');
-      } else if (isAdmin === 'user'){
-        navigate('/dashboard/my-players');
+        if (requires2f) {
+          setNeedsVerificationCode(true);
+          toast.info("Por favor, ingresa tu código de verificación.");
+        } else if (token) {
+          handleCookie(token, isAdmin, name);
+          toast.success(message);
+
+          setTimeout(() => {
+            if (isAdmin === 'admin') {
+              navigate('/dashboard/users');
+            } else if (isAdmin === 'user') {
+              navigate('/dashboard/my-players');
+            }
+          }, 2000);
+        }
       }
-    }, 2000);
-  }
-} catch (error) {
-  if (error.response) {
-    console.error("Error al iniciar sesión:", error.response.data.message || 'Error en el inicio de sesión');
-
-    if (error.response.status === 429) {
-      toast.error("Demasiados intentos de inicio de sesión. Por favor, inténtelo de nuevo más tarde.");
-    } else {
-      toast.error("Error en inicio de sesión");
+    } catch (error) {
+      toast.error("Error en el inicio de sesión. Por favor, inténtalo de nuevo.");
     }
-  } else if (error.request) {
-    console.error("Error al iniciar sesión:", 'No se pudo conectar con el servidor');
-    toast.error("Error en inicio de sesión");
-  } else {
-    console.error("Error al iniciar sesión:", 'Error al procesar la solicitud de inicio de sesión');
-    toast.error("Error en inicio de sesión");
-  }
-}
-};
-  
+  };
+
   return (
-    <div    
-      style={{
-      maxWidth: "100%",
-      height: "auto",
-      filter: "drop-shadow(10px 10px 12px rgba(90, 90, 0, 0.8))",
-    }}
-    className=" bg-gray-200 min-h-screen flex items-center justify-center">
-      <div className="bg-white p-8 rounded-xl shadow-md w-full max-w-md mb-12 " style={{ backgroundColor: "#F2F2F2" }}>
+    <div className="bg-gray-200 min-h-screen flex items-center justify-center">
+      <div className="bg-white p-8 rounded-xl shadow-md w-full max-w-md mb-12" style={{ backgroundColor: "#F2F2F2" }}>
         <h2 className="text-3xl font-semibold text-center mb-6">Iniciar sesión</h2>
         <form className="space-y-4" onSubmit={handleSubmit}>
           <div className="flex flex-col items-center">
@@ -99,8 +228,8 @@ const Logincomponent = ({ onFormSwitch }) => {
           <div className="flex flex-col items-center">
             <label htmlFor="password" className="block mb-1 text-center font-bold">Contraseña</label>
             <input 
-              value={pass} 
-              onChange={(e) => setPass(e.target.value)} 
+              value={password} 
+              onChange={(e) => setPassword(e.target.value)} 
               type="password" 
               placeholder="******" 
               autoComplete="off"
@@ -112,13 +241,29 @@ const Logincomponent = ({ onFormSwitch }) => {
               style={{ borderRadius: "5px" }} 
             />
           </div>
+          {needsVerificationCode && ( // Renderizado condicional del código de verificación
+            <div className="flex flex-col items-center">
+              <label htmlFor="verificationCode" className="block mb-1 text-center font-bold">Código de verificación</label>
+              <input 
+                value={verificationCode} 
+                onChange={(e) => setVerificationCode(e.target.value)} 
+                type="text" 
+                placeholder="Ingrese el código recibido" 
+                id="verificationCode" 
+                autoComplete="off"
+                name="verificationCode" 
+                required
+                className="input w-full max-w-xs rounded-xl"  
+                style={{ borderRadius: "5px" }} 
+              />
+            </div>
+          )}
           <div className="flex justify-center">
             <button 
-              className="button-login  bg-yellow-l hover:bg-yellow-d text-black font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline" 
+              className="button-login bg-yellow-l hover:bg-yellow-d text-black font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline" 
               type="submit" 
-              
             >
-              Iniciar sesión
+              {needsVerificationCode ? "Verificar código" : "Iniciar sesión"}
             </button>
           </div>
         </form>
@@ -132,6 +277,8 @@ const Logincomponent = ({ onFormSwitch }) => {
       </div>
     </div>
   );
-}
+};
 
-export default Logincomponent;
+export default LoginComponent;
+
+
