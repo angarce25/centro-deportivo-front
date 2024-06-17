c77c6085a47f2b7746dd19153740d08654479fa8
Isarok <isaac.lucerito@gmail.com>
2024-06-17 18:43:43 +0200

    frontend_feature/products_addedStylesOnOrdersDetails

--- src/pages/OrderDetails.jsx
+++ src/pages/OrderDetails.jsx
@@ -44,41 +44,47 @@ const OrderDetailsCard = () => {
   const { order, productInfo } = orderDetails;
 console.log (order, productInfo)
   return (
-    <section className="m-10 w-150 bg-base-100 shadow-l flex flex-col md:flex-row justify-between">
-    <div className="m-10 flex-1 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-      <h4 className="card-title mb-5 col-span-full">Detalles del Pedido</h4>
+    <section className="max-w-7xl overflow-y-auto max-h-[90vh]">
+  <div className="m-10 flex-1 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+    <h4 className="card-title mb-5 col-span-full">Detalles del Pedido</h4>
 
-      <div className="col-span-full">
-        <p className="mb-1 font-semibold">Fecha del pedido: {new Date(order.createdAt).toLocaleDateString()}</p>
-        <p className="mb-1 font-semibold">Nombre del Cliente: {order.user_id && `${order.user_id.name} ${order.user_id.lastname}`}</p>
-        <p className="mb-1 font-semibold">Email del Cliente: {order.user_id && order.user_id.email}</p>
-        <p className="font-semibold">Total del Pedido: {order.resume.total}€</p>
-        {order.status && <p className="font-semibold">Estado del Pedido: {order.status}</p>}
-      </div>
-
-      <h4 className="card-title mb-5 col-span-full">Productos</h4>
-      <table className="table-fixed w-full col-span-full">
-        <thead>
-          <tr>
-            <th className="w-1/3 px-4 py-2">Nombre</th>
-            <th className="w-1/4 px-4 py-2">Talla</th>
-            <th className="w-1/4 px-4 py-2">Precio</th>
-            <th className="w-1/6 px-4 py-2">Cantidad</th>
-          </tr>
-        </thead>
-        <tbody>
-          {productInfo && productInfo.map((product, index) => (
-            <tr key={product._id}>
-              <td className="border px-4 py-2">{product.name}</td>
-              <td className="border px-4 py-2">{order.selectedSize && order.selectedSize[index]}</td>
-              <td className="border px-4 py-2">{product.price}€</td>
-              <td className="border px-4 py-2">{order.resume.quantity && order.resume.quantity[index]}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div className="col-span-full">
+      <p className="mb-1 font-semibold">Fecha del pedido: {new Date(order.createdAt).toLocaleDateString()}</p>
+      <p className="mb-1 font-semibold">Nombre del Cliente: {order.user_id && `${order.user_id.name} ${order.user_id.lastname}`}</p>
+      <p className="mb-1 font-semibold">Email del Cliente: {order.user_id && order.user_id.email}</p>
+      <p className="font-semibold">Total del Pedido: {order.resume.total}€</p>
+      {order.status && <p className="font-semibold">Estado del Pedido: {order.status}</p>}
     </div>
-  </section>
+
+    <h4 className="card-title mb-5 col-span-full">Productos</h4>
+    <div className="flex flex-col mt-6 col-span-full">
+      <div className="-my-2 overflow-x-auto">
+        <div className="align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200">
+          <table className="table table-zebra min-w-full">
+            <thead>
+              <tr className="text-gray-800 text-sm">
+                <th className="px-6 py-6 bg-white text-center text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider border-black">Nombre</th>
+                <th className="px-6 py-6 bg-white text-center text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider border-black">Talla</th>
+                <th className="px-6 py-6 bg-white text-center text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider border-black">Precio</th>
+                <th className="px-6 py-6 bg-white text-center text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider border-black">Cantidad</th>
+              </tr>
+            </thead>
+            <tbody>
+              {productInfo && productInfo.map((product, index) => (
+                <tr key={product._id} className="border-black">
+                  <td className="px-4 py-4 whitespace-no-wrap border-b border-black text-center">{product.name}</td>
+                  <td className="px-4 py-4 whitespace-no-wrap border-b border-black text-center">{order.selectedSize && order.selectedSize[index]}</td>
+                  <td className="px-4 py-4 whitespace-no-wrap border-b border-black text-center">{product.price}€</td>
+                  <td className="px-4 py-4 whitespace-no-wrap border-b border-black text-center">{order.resume.quantity && order.resume.quantity[index]}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
   );
 };
 
